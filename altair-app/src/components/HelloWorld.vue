8af7c046af1a938f1e2d50d469edadca8fc7177e
Ji Xu <ji@motivodata.com>
2018-11-06 10:01:48 -0800

    layout playaround

--- altair-app/src/components/HelloWorld.vue
+++ altair-app/src/components/HelloWorld.vue
@@ -1,4 +1,50 @@
 
+<template>
+<v-app>
+  <v-content>
+  <v-container fluid grid-list-lg>
+      <v-layout row>
+        <v-layout col wrap fill-height>
+          <v-flex d-flex>
+          <!-- <div> -->
+            <v-card>
+              <!-- <v-responsive :aspect-ratio="16/9"> -->
+              <v-card-text>1</v-card-text>
+              <div id="vega-box"></div>
+              <v-btn @click.native="reloadImage()">Reload</v-btn>
+              <!-- </v-responsive> -->
+            </v-card>
+          <!-- </div> -->
+          </v-flex>
+
+          <v-flex d-flex>
+          <!-- <div> -->
+            <v-card>
+              <v-card-text>2</v-card-text>
+              <div id="vega-box2"></div>
+              <v-btn @click.native="reloadImage2()">Reload</v-btn>
+            </v-card>
+          <!-- </div> -->
+          </v-flex>
+        </v-layout>
+
+        <v-flex d-flex>
+          <v-card>
+            <v-card-text>3</v-card-text>
+            
+          </v-card>
+
+        </v-flex>
+      </v-layout>
+    
+  </v-container>
+  </v-content>
+</v-app>
+</template>
+
+
+
+
 <script>
 import {default as vegaEmbed} from 'vega-embed'
 
@@ -10,37 +56,21 @@ export default {
           vegaEmbed('#vega-box', spec, {actions: false})
         })
       })
+    },
+    reloadImage2 () {
+      fetch('/vega-example').then(response => {
+        response.json().then(spec => {
+          vegaEmbed('#vega-box2', spec, {actions: false})
+        })
+      })
     }
   }
 }
 </script>
 
 
-<template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-left>
-        <v-btn @click.native="reloadImage()">Reload</v-btn>
-        <div id="vega-box"></div>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
-</template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
